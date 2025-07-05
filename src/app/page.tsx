9ab70c9d1517f2698cf6713d6f1ebf13c19c24e7
Mohid Faizi <mohidfaizipro@gmail.com>
2025-07-05 20:23:10 +0500

    Adding the TRPC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
-export default function Home() {
+import { caller } from "@/trpc/server";
+
+export default async function Home() {
+  const data = await caller.hello({
+    text: "world",
+  });
+
   return (
     <>
+      <div>{data.greeting}</div>
     </>
   );
 }
